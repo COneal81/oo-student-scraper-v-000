a48480bf1362ee81a3a95a60bcaf799b24c0e72e
Kari O'Neal <triadfirewood@gmail.com>
2018-09-06 13:41:42 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,21 +19,21 @@ end
 
 
   def self.scrape_profile_page(profile_url)
-    	student_profile = {}
+    	student_list = {}
       html = Nokogiri::HTML(open(profile_url))
     
       html.css("div.main-wrapper.profile.social-icon-container a").each do |student|
        if student.attribute("href").value.include?("twitter")
-         student_profile[:twitter] = student.attribute("href").value
+         student_list[:twitter] = student.attribute("href").value
       elsif student.attribute("href").value.include?("linkedin")
-      student_profile[:linkedin] = student.attribute("href").value
+      student_list[:linkedin] = student.attribute("href").value
       elsif student.attribute("href").value.include?("github")
-      student_profile[:github] = student.attribute("href").value 
-    else student_profile[:blog] = student.attribute("href").value
+      student_list[:github] = student.attribute("href").value 
+    else student_list[:blog] = student.attribute("href").value
   end
 end
-  student_profile[:profile_quote] = html.css("div.profile-quote").text
-  student_profile[:bio] = html.css("div.bio-content.content-holder p").text 
-  student_profile
+  student_list[:profile_quote] = html.css("div.profile-quote").text
+  student_list[:bio] = html.css("div.bio-content.content-holder p").text 
+  student_list
 end
 end
