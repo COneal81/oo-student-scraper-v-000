b7feccc9e87c69921e026ac81a4ed698d386fb1b
Kari O'Neal <triadfirewood@gmail.com>
2018-09-06 12:56:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ end
       html.css("div.social-icon-controler a").each do |student|
        if student.attribute("href").value.include?("twitter")
          student_list[:twitter] = student.attribute("href").value
-      elsif student.attribute("href")value.include?("linkedin")
+      elsif student.attribute("href").value.include?("linkedin")
       student_list[:linkedin] = student.attribute("href").value
       elsif student.attribute("href").value.include?("github")
       student_list[:github] = student.attribute("href").value 
