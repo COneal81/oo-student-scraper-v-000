d66b71ea559394abc3465d4b94effa91100de683
Kari O'Neal <triadfirewood@gmail.com>
2018-09-06 12:45:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,6 +29,7 @@ end
         students_list[:linkedin] = url if url.include?("linkedin")
         students_list[:github] = url if url.include?("github")
         students_list[:blog] = url if student.css("img").attribute("src").text.include?("rss")
+      end
     end
         students_list[:profile_quote] = html.css("div.profile-quote").text
         students_list[:bio] = html.css("div.bio-content p").text
