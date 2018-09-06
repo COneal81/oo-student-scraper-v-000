388d55651afacbca396dcc5b140b9a317d3cf968
Kari O'Neal <triadfirewood@gmail.com>
2018-09-06 13:34:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,8 +20,11 @@ class Student
     
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key=""), value)
   end
+  self
+end
 
   def self.all
     @@all
