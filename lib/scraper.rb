399f8a4d30c5be9fe23596af6029474917dd3032
Kari O'Neal <triadfirewood@gmail.com>
2018-09-06 11:06:36 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,30 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
+    student_list = []
+        html.css(".student-card").collect do |student|
+      student_hash = {
+            name: student.css(".student-name").text,
+            location: student.css(".student-location").text,
+            profile_url: student.css("a").attribute("href").value
+      }
+            student_list << student_hash
+        end
+    student_list
   end
 
+
   def self.scrape_profile_page(profile_url)
+    html = Nokogiri::HTML(open(profile_url))
+    
+    html.css(".   ")
+    student_profile_list = {
+      student_list[:twitter_url] url if url.include?("twitter")
+      student_list[:linkedin_url] url if url.include?("linkedin")
+      student_list[:github_url] url if url.include?("github")
+      
+    }
     
   end
 
