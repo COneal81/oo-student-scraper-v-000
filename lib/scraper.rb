ba5d7960f974208705800be983bf68e5f2ebfe42
Kari O'Neal <triadfirewood@gmail.com>
2018-09-06 12:55:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,5 +32,8 @@ end
     else student_list[:blog] = student.attribute("href").value
   end
 end
-  student_list[:profile_quote] = html.css("")
+  student_list[:profile_quote] = html.css("div.profile-quote").text
+  student_list[:bio] = html.css("div.bio-content p").text 
+  student_list
+end
     
